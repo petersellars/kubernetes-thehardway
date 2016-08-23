ab062249e3001b7b99b513d960408cd2b7278801
Pete Sellars <psellars@gmail.com>
2016-08-23 21:30:28 +1200

    add create/clean for firewall-rules and public IP Address

--- scripts/clean-up.sh
+++ scripts/clean-up.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Delete Kubernetes Public IP Address
+gcloud compute addresses delete kubernetes --region us-central1 --quiet
+
+# Delete Firewall Rules
+gcloud compute firewall-rules delete \
+  kubernetes-allow-api-server \
+  kubernetes-allow-healthz \
+  kubernetes-allow-ssh \
+  kubernetes-allow-rdp \
+  kubernetes-allow-internal \
+  kubernetes-allow-icmp \
+  --quiet
+
 # Delete Kubernetes Network Subnet
 gcloud compute networks subnets delete kubernetes --region us-central1 --quiet
 
--- scripts/cloud-infra-provisioning.sh
+++ scripts/cloud-infra-provisioning.sh
@@ -9,3 +9,40 @@ gcloud compute networks subnets create kubernetes \
   --network kubernetes \
   --range 10.240.0.0/24 \
   --region us-central1
+
+# Create Firewall Rules
+gcloud compute firewall-rules create kubernetes-allow-icmp \
+  --allow icmp \
+  --network kubernetes \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules create kubernetes-allow-internal \
+  --allow tcp:0-65535,udp:0-65535,icmp \
+  --network kubernetes \
+  --source-ranges 10.240.0.0/24
+
+gcloud compute firewall-rules create kubernetes-allow-rdp \
+  --allow tcp:3389 \
+  --network kubernetes \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules create kubernetes-allow-ssh \
+  --allow tcp:22 \
+  --network kubernetes \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules create kubernetes-allow-healthz \
+  --allow tcp:8080 \
+  --network kubernetes \
+  --source-ranges 130.211.0.0/22
+
+gcloud compute firewall-rules create kubernetes-allow-api-server \
+  --allow tcp:6443 \
+  --network kubernetes \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules list --filter "network=kubernetes"
+
+# Create the Kubernetes Public IP Address
+gcloud compute addresses create kubernetes --region us-central1
+gcloud compute addresses list kubernetes
